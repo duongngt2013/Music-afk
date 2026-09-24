3bc0eb9ea9329239cd8977f8a941c00d6a09f50f
duongngt2013 <duongngt2013@gmail.com>
2026-09-24 16:55:34 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,4 +85,4 @@ async def stop(ctx):
         await ctx.send("👋 Đã ngắt kết nối!")
 
 # Token của bạn
-bot.run('MTU1MjU4Mjc1Mzg2ODg0NTEwNg.GBtZ4Y.Vgb4eoiR0EGRbzCBRqxGUz82lqyJKJ-8KL6vBQ')
+bot.run(os.getenv('DISCORD_TOKEN'))
